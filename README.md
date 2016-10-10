76e2789e5d48cb14644de65212e080f1476b10e4
Casey Olson <casey@bookcasey.com>
2016-10-10 18:58:18 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Homebridge GPIO Intercom
+
+Modify your intercom panel to open the building door using Raspberry Pi GPIO and Homebridge
+
+Todo: add credit where credit is due
+Todo: add instructions/schematics
---
+++ index.js
@@ -0,0 +1,77 @@
+var wpi = require("wiring-pi");
+
+var Service, Characteristic;
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("homebridge-gpio-intercom", "Intercom", LockAccessory);
+}
+function IntercomAccessory(log, config) {
+  this.log = log;
+  this.name = config["name"];
+  this.pin = config['pin'];
+  this.duration = config['duration'];
+  this.service = new Service.LocMechanism(this.name);
+
+  if (!this.pin) throw new Error('You must provide a config value for pin.');
+  if(!this.duration) throw new Error('You must provide a duration');
+  if(!is_int(this.duration) && this.duration > 0) throw new Error('Duration must be a positive integer');
+
+  wpi.setup('sys');
+  wpi.pinMode(this.pin, wpi.OUTPUT);
+
+  this.service
+    .getCharacteristic(Characteristic.LockTargetState)
+    .on('set', this.setState.bind(this));
+}
+IntercomAccessory.prototype.setState = fusnction (state, callback) {
+    var self = this;
+    callback = callback || function() {};
+    if (this.lockTimer) {
+        clearTimeout(this.lockTimer);
+        delete this.lockTimer;
+    }
+
+    var currentState;
+
+    if (state == Characteristic.LockTargetState.SECURED) {
+      this.pinAction(0);
+      currentState = Characteristic.LockCurrentState.SECURED;
+    } else {
+      this.pinAction(1);
+      currentState = Characteristic.LockCurrentState.UNSECURED;
+    }
+
+    this.service
+        .setCharacteristic(Characteristic.LockCurrentState, currentState);
+
+    if (state == Characteristic.LockTargetState.UNSECURED) {
+        this.lockTimer = setTimeout(
+            function(caller) {
+                self.pinAction(0);
+                caller.service.getCharacteristic(Characteristic.LockTargetState).updateValue(Characteristic.LockTargetState.SECURED);
+                caller.service.getCharacteristic(Characteristic.LockCurrentState).updateValue(Characteristic.LockCurrentState.SECURED);
+                console.log('timer over');
+            },
+            this.duration,
+            this
+        );
+    }
+    callback();
+}
+IntercomAccessory.prototype.getServices = function() {
+  return [this.service];
+}
+
+IntercomAccessory.prototype.pinAction = function(action) {
+    this.log('Turning ' + (action == 1 ? 'on' : 'off') + ' pin #' + this.pin);
+
+    var self = this;
+    wpi.digitalWrite(self.pin, action);
+    var success = (wpi.digitalRead(self.pin) == action);
+    return success;
+}
+
+var is_int = function(n) {
+    return n % 1 === 0;
+}
